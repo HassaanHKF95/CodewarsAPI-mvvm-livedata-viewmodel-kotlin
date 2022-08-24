b395ac7cbcf4ff0b01380c4f674ca7e8d333ed47
Hassan-cmg <hassan.kaleem@createmedia-group.com>
2022-08-24 17:55:53 +0500

    MVVM setup - challenges list of  'Voile' user  API call with LiveData

---
+++ app/src/main/java/com/hassan/codewars/data/repositories/CodeWarRepository.kt
@@ -0,0 +1,43 @@
+package com.hassan.codewars.data.repositories
+
+import com.hassan.codewars.data.retrofit.client.RetrofitRestClient
+import com.hassan.codewars.data.retrofit.endpoints.ListOfCompletedChallengesAPI
+import com.hassan.codewars.presentation.models.ChallengesListModel
+import com.hassan.codewars.presentation.models.ChallengesModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.LiveData
+
+
+open class CodeWarRepository {
+
+    private val challengesResponseLiveData = MutableLiveData<ChallengesModel>()
+
+    val mApiInterface =
+        RetrofitRestClient.getRetrofit().create(ListOfCompletedChallengesAPI::class.java)
+
+
+    fun getChallengesListAPI(page: String) {
+        mApiInterface.getListOfCompletedChallengesAPI(page)
+            .enqueue(object : Callback<ChallengesModel> {
+                override fun onResponse(
+                    call: Call<ChallengesModel>,
+                    response: Response<ChallengesModel>
+                ) {
+                    if (response.body() != null)
+                        challengesResponseLiveData.value = response.body()
+                }
+
+                override fun onFailure(call: Call<ChallengesModel>, t: Throwable) {
+                    challengesResponseLiveData.value = null
+                }
+            })
+    }
+
+    open fun getchallengesResponseLiveData(): LiveData<ChallengesModel>? {
+        return challengesResponseLiveData
+    }
+
+}
---
+++ app/src/main/java/com/hassan/codewars/presentation/models/viewModels/ChallengesListViewModel.kt
@@ -0,0 +1,24 @@
+package com.hassan.codewars.presentation.models.viewModels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import com.hassan.codewars.data.repositories.CodeWarRepository
+import com.hassan.codewars.presentation.models.ChallengesModel
+
+
+class ChallengesListViewModel : ViewModel() {
+
+    private val challengesRepository: CodeWarRepository = CodeWarRepository()
+    private var challengesModelLiveData: LiveData<ChallengesModel>? = null
+
+    fun init() {
+        challengesModelLiveData = challengesRepository.getchallengesResponseLiveData()
+    }
+    fun callChallengesAPI(page: String){
+        challengesRepository.getChallengesListAPI(page)
+    }
+
+    fun getchallengesModelLiveData(): LiveData<ChallengesModel>? {
+        return challengesModelLiveData
+    }
+}
